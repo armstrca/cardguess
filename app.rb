9ddb0aeae740b5e3fdee90510ad0db4615a11ea0
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-08-18 20:48:28 +0000

    Trying to get my boy to remember!

--- app.rb
+++ app.rb
@@ -8,19 +8,13 @@ require "sinatra/cookies"
 require "better_errors"
 enable :sessions
 
+OPENAI_API_KEY = ENV.fetch("OPENAI_API_KEY")
+
+
 $card_values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 $suits_all = ["♣︎", "♠︎", "♥︎", "♦︎"]
 
 @deck_hash = {}
-  
-# trying to write something that makes the correct cards go to the correct piles. So pile 1 should follow the pattern of 1, 5, 9, 13, 17; pile 2 would be 2, 6, 10, 14, 18; etc..
-=begin
-def pile2 
-  $dealer_deck
-  $dealer_deck_index += 1
-  $deal = $dealer_deck[$dealer_deck_index.step(2..4)]
-end
-=end
 
 
 $suits_all.each do |suit|
@@ -34,19 +28,18 @@ $diamonds_array = @deck_hash["♦︎"]
 $deck_array = []
 $deck_array.concat($clubs_array, $spades_array, $hearts_array, $diamonds_array)
 $deck_array.push("Joker", "Joker")
-@dealer_deck = $deck_array.sample
+$dealer_deck = $deck_array.sample
 
-Num = 13
-#Num = rand 15..405
+Num = rand 30..223
 $dealer_deck = []
 
 (Num.to_i).times do
   $dealer_deck = $dealer_deck.push($deck_array.sample)
 end
-pp $dealer_deck.length
+$dealer_deck.length
 
 $dealer_deck = $dealer_deck.concat($deck_array)
-pp $dealer_deck.length
+$dealer_deck.length
 def distribute_to_piles(deck)
   piles = Array.new(4) { [] }
 
@@ -57,75 +50,29 @@ def distribute_to_piles(deck)
   piles
 end
 
-@piles = distribute_to_piles($dealer_deck)
-
-# code that loops through each individual array in @piles 1 by 1,
-# only displaying/dealing the card most recently dealt in each pile
-# def full_pile_deal(piles, current_card_indices)
-#   result = ""
-#   piles.each_with_index do |pile, pile_index|
-#       current_card_index = current_card_indices[pile_index]
-#       current_card = pile[current_card_index]
-#       result += "Pile #{pile_index + 1}: #{current_card}<br>"
-
-#       current_card_indices[pile_index] = (current_card_index + 1) % pile.length
-#   end
-#   result
-# end
+@piles = distribute_to_piles($dealer_deck.shuffle!)
 
 @pile1 = @piles[0] 
 @pile2 = @piles[1]
 @pile3 = @piles[2]
 @pile4 = @piles[3]
+current_card_indices = [-1, -1, -1, -1] 
 
-# def proper_dealing_order
-#   @pile1 = @piles[0] 
-#   @pile2 = @piles[1]
-#   @pile3 = @piles[2]
-#   @pile4 = @piles[3]
-#   while !@pile1.empty? || !@pile4.empty?
-#   if @pile1.length == @pile4.length
-#     @pile1.shift
-#     pp @piles
-#   end
-#   if @pile2.length > @pile1.length
-#     @pile2.shift
-#     pp @piles
-#   end
-#   if @pile3.length > @pile2.length
-#     @pile3.shift
-#     pp @piles
-#   end
-#   if @pile4.length > @pile3.length
-#     @pile4.shift
-#     pp @piles
-#   end
-#   # if @pile1.length.nil? && @pile4.length.nil?
-#   #   puts "Game over!"
-#   end
-#   puts "Game over!"
-# end
-
-piles = [@piles[0], @piles[1], @piles[2], @piles[3]]  # Your array of piles
-current_card_indices = [-1, -1, -1, -1]  # Initialize the current card index for each pile
-
-# Call the function to display the next card from each pile
-# full_pile_deal(piles, current_card_indices)
-
-
-"9999999999999999999999999999999999999999999999999999999999999" 
 pp @pile1.length
 pp @pile2.length
 pp @pile3.length
 pp @pile4.length
 
 get("/") do
+  session[:cookiepile] ||= []
   erb(:home)
 end
 
+
 $current_step_index = 0
 
-get("/next_card") do
+post("/next_card") do
+  session[:cookiepile] ||= []
   $current_step_index += 0
   $dealer_deck_index = 0
   $deal = $dealer_deck[$dealer_deck_index]
@@ -140,12 +87,16 @@ get("/next_card") do
   case $current_step_index
   when 0
     @pile1.shift if @pile1.length > 1
+    pp session[:cookiepile] += [@pile1.first]
   when 1
     @pile2.shift if @pile2.length > 1
+    pp session[:cookiepile] += [@pile2.first]
   when 2
     @pile3.shift if @pile3.length > 1
+    pp session[:cookiepile] += [@pile3.first]
   when 3
     @pile4.shift if @pile4.length > 1
+    pp session[:cookiepile] += [@pile4.first]
   end
 
   # Increment the index for the next step
@@ -153,11 +104,12 @@ get("/next_card") do
   $current_step_index %= 4
   # Store the updated @piles array back into the session
   session[:piles] = @piles
-
+  session[:cookiepile]
   erb(:next_card)
 end
 
-get("/first_pile") do
+post("/first_pile") do
+  session[:cookiepile] ||= []
   $current_step_index += 0
   $dealer_deck
   @piles = session[:piles] || distribute_to_piles($dealer_deck)
@@ -172,11 +124,13 @@ get("/first_pile") do
   $current_step_index %= 4
   # Store the updated @piles array back into the session
   session[:piles] = @piles
-  pp @pile1
+  pp session[:cookiepile] += [@pile1.first]
+  @pile1
   erb(:first_pile)
 end
 
-get("/second_pile") do
+post("/second_pile") do
+  session[:cookiepile] ||= []
   $dealer_deck
   @piles = session[:piles] || distribute_to_piles($dealer_deck)
   @current_card_indices = current_card_indices
@@ -190,11 +144,13 @@ get("/second_pile") do
   $current_step_index %= 4
   # Store the updated @piles array back into the session
   session[:piles] = @piles
-  pp @pile2
+  pp session[:cookiepile] += [@pile2.first]
+  @pile2
   erb(:second_pile)
 end
 
-get("/third_pile") do
+post("/third_pile") do
+  session[:cookiepile] ||= []
   $dealer_deck
   @piles = session[:piles] || distribute_to_piles($dealer_deck)
   @current_card_indices = current_card_indices
@@ -208,11 +164,13 @@ get("/third_pile") do
   $current_step_index %= 4
   # Store the updated @piles array back into the session
   session[:piles] = @piles
-  pp @pile3
+  pp session[:cookiepile] += [@pile3.first]
+  @pile3
   erb(:third_pile)
 end
 
-get("/fourth_pile") do
+post("/fourth_pile") do
+  session[:cookiepile] ||= []
   $dealer_deck
   @piles = session[:piles] || distribute_to_piles($dealer_deck)
   @current_card_indices = current_card_indices
@@ -226,23 +184,110 @@ get("/fourth_pile") do
   $current_step_index %= 4
   # Store the updated @piles array back into the session
   session[:piles] = @piles
-  pp @pile4
+
+  pp session[:cookiepile] += [@pile4.first]
+  @pile4
   erb(:fourth_pile)
 end
 
 get("/new_deck") do
+  $card_values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+  $suits_all = ["♣︎", "♠︎", "♥︎", "♦︎"]
+
+  @deck_hash = {}
+
+
+  $suits_all.each do |suit|
+    @deck_hash[suit] = $card_values.map { |value| "#{suit}#{value}"}
+  end
+
+  $clubs_array = @deck_hash["♣︎"] 
+  $spades_array = @deck_hash["♠︎"] 
+  $hearts_array = @deck_hash["♥︎"] 
+  $diamonds_array = @deck_hash["♦︎"] 
+  $deck_array = []
+  $deck_array.concat($clubs_array, $spades_array, $hearts_array, $diamonds_array)
+  $deck_array.push("Joker", "Joker")
+  $dealer_deck = $deck_array.sample
+
+  Num = rand 30..223
+  $dealer_deck = []
+
   (Num.to_i).times do
     $dealer_deck = $dealer_deck.push($deck_array.sample)
   end
+  pp $dealer_deck.length
 
   $dealer_deck = $dealer_deck.concat($deck_array)
+  pp $dealer_deck.length
+  def distribute_to_piles(deck)
+    piles = Array.new(4) { [] }
 
-  @piles = distribute_to_piles($dealer_deck)
-  @pile1 = @piles[0] 
-  @pile2 = @piles[1]
-  @pile3 = @piles[2]
-  @pile4 = @piles[3]
-  
+    deck.each_with_index do |card, index|
+      pile_index = index % 4
+      piles[pile_index] << card
+    end
+    piles
+  end
+
+  @piles = distribute_to_piles($dealer_deck.shuffle!)
+  session[:cookiepile1] ||= []
+  session[:cookiepile2] ||= []
+  session[:cookiepile3] ||= []
+  session[:cookiepile4] ||= []
   redirect to ("/")
   erb(:home)
+end
+
+post("/chat") do
+    
+  pp  @chat_history = JSON.parse(cookies[:chat_history] || "[]")
+
+  pp  @current_message = params.fetch("user_chat_msg")
+
+  pp  @chat_history << { "role" => "user", "content" => @current_message }
+
+  pp  request_headers_hash = {
+      "Authorization" => "Bearer #{ENV.fetch("OPENAI_API_KEY")}",
+      "content-type" => "application/json"
+    }
+
+  pp request_messages = [
+      {
+        "role" => "system",
+        "content" => "You are a helpful assistant."
+      },
+      {
+        "role" => "user",
+        "content" => @current_message
+      }
+    ]
+
+  @chat_history.each do |message|
+      request_messages << {
+        "role" => message["role"],
+        "content" => message["content"]
+      }
+    end
+
+  pp  request_body_hash = {
+      "model" => "gpt-3.5-turbo",
+      "messages" => request_messages
+    }
+
+    request_body_json = JSON.generate(request_body_hash)
+
+    raw_response = HTTP.headers(request_headers_hash).post(
+      "https://api.openai.com/v1/chat/completions",
+      :body => request_body_json
+    ).to_s
+
+    @parsed_response = JSON.parse(raw_response)
+
+    @reply = @parsed_response.dig("choices", 0, "message", "content")
+
+    @chat_history << { "role" => "assistant", "content" => @reply }
+
+    cookies[:chat_history] = JSON.generate(@chat_history)
+    erb(:chat)
 end
